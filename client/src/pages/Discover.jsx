df1c546e6ed816fa2ad244dd568613d94624600b
BForoughi <147166034+BForoughi@users.noreply.github.com>
2026-03-30 17:51:24 +0100

    adding slogan and comic fan image

--- client/src/pages/Discover.jsx
+++ client/src/pages/Discover.jsx
@@ -1,11 +1,17 @@
 import AppNavbar from "../components/Navbar"
 import Banner from "../components/Banner"
+import landingPageComics from '../assets/landingpageComics.png'
 
 function Discover(){
     return(
         <>
             <AppNavbar/>
             <Banner />
+            <p id="slogan">Discover Your Favourite Comic Book Series</p>
+            <div id="landingcomics-container" className="d-flex justify-content-center">
+                <img src={landingPageComics} alt="Five randomly chosen comics from a couple different publishers fanned out together" id="landingPageComics"/>
+            </div>
+            
         </>
         
     )
