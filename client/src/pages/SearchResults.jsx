c36cabc44ce53e428a202740ecaaa58a53504de0
BForoughi <147166034+BForoughi@users.noreply.github.com>
2026-04-19 19:33:05 +0100

    creating a searchbar that allows the user to edit search and creating responsive styling for the results page

--- client/src/pages/SearchResults.jsx
+++ client/src/pages/SearchResults.jsx
@@ -6,10 +6,20 @@ import { useSearchParams } from "react-router-dom";
 function SearchResultsPage (){
     const [results, setResults] = useState([]);
     // had to grab this from claude as i was getting the wrong results returned
-    const [searchParams] = useSearchParams();
-    const query = searchParams.get('q'); // reads ?q=batman from the URL
+    const [searchParams, setSearchParams] = useSearchParams();
+    const query = searchParams.get('q'); // reads ?q="user's search" from the URL
+    
+    const [inputValue, setInputValue] = useState(query || ''); // this is used to store the orginal search to allow for an edit search
+
+    const handleSearch = (e) => {
+        if (e.key === 'Enter' && e.target.value.length > 2) {
+            setSearchParams({ q: e.target.value }); // updates the URL which triggers useEffect
+        }
+    };
 
     useEffect(() => {
+        setInputValue(query || ''); // sync input when URL changes
+
         if (!query) return;
 
         const fetchData = async () => {
@@ -28,19 +38,28 @@ function SearchResultsPage (){
         <>
             <div id="search-results_container">
                 <AppNavbar/>
-                <ul className='search-results'>
-                    {results.map((item, i) => (
-                        <li key={i} className='search-result'>
-                            <img 
-                                src={item.image?.icon_url} 
-                                alt={item.name}
-                                className='search-result-img'
-                            />
-                            <span className='result-name'> {item.name}, {item.volume?.name} - </span>
-                            <span className='result-type'>{item.resource_type}</span>
-                        </li>
-                    ))}
-                </ul>
+                <div id="search-results_inner">
+                    <input
+                        type='text'
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                        onKeyDown={handleSearch}
+                        className='search-results_searchbar'
+                    />
+                    <ul className='search-results'>
+                        {results.map((item, i) => (
+                            <li key={i} className='search-result'>
+                                <img 
+                                    src={item.image?.icon_url} 
+                                    alt={item.name}
+                                    className='search-result-img'
+                                />
+                                <span className='result-name'> {item.name}, {item.volume?.name}</span>
+                                {/* <span className='result-type'>{item.resource_type}</span> */}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         
         </>
